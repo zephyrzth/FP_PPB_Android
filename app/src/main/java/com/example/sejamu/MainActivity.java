10c9fae44f08ecb8ea1a1478d3ea06217e88e74a
Anargya Widyadhana <anargyawidyadhana@gmail.com>
2021-01-10 22:20:39 +0700

    move deteksi to fragment

--- app/src/main/java/com/example/sejamu/MainActivity.java
+++ app/src/main/java/com/example/sejamu/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.sejamu;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,40 +16,34 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView menu_bawah;
-    private Button deteksi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        openFragment(DeteksiFragment.newInstance("", ""));
         menu_bawah = findViewById(R.id.menu_bawah);
-        deteksi = findViewById(R.id.bDeteksi);
         menu_bawah.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
                 switch (item.getItemId()){
-                    case R.id.katalog :
+                    case R.id.deteksi:
+                        openFragment(DeteksiFragment.newInstance("", ""));
                         break;
-                    case R.id.belanja :
+                    case R.id.belanja:
                         break;
-                    case R.id.profil :
+                    case R.id.profil:
                         break;
                 }
                 return true;
             }
         });
+    }
 
-        deteksi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                switch (view.getId()){
-                    case R.id.bDeteksi:
-                        Intent intent = new Intent(MainActivity.this, ClassifierActivity.class);
-                        startActivity(intent);
-                        break;
-                }
-            }
-        });
+    private void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
